765d11a5197a2da22f1b304fc278a94d79067da9
hsuntarq <hsuntariq@gmail.com>
2023-03-23 15:49:37 +0500

    Cart

--- Cart.jsx
+++ Cart.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import { UseGlobalContext } from './Context'
+import Nav from './Nav';
 const Cart = () => {
+    const { data,add } = UseGlobalContext();
 return (
     <>
-        <h1>hello</h1>
+    <Nav/>
+        <div className="row d-flex justify-content-center align-items-center">
+            {data.map((item) => {
+                return(
+                    <>
+                        <div key={item.id} className="col-md-3 col-sm-6 mb-2">
+                            <img style={{width:'200px',height:'200px',objectFit:'cover'}} src={item.image} alt="" />
+                            <h3>{item.name}</h3>
+                            <p className="text-secondary text-justify">
+                                {item.info}
+                            </p>
+                            <button onClick={()=>add(item.id)} className="btn btn-dark w-100">
+                                Buy
+                            </button>
+                        </div>
+                    </>
+                )
+            })}
+        </div>
     </>
 )
 }
